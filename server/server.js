96801e97092a30d9f12ccbc2c4836ea2dc1c604a
Matt Felsen <mattfelsen@gmail.com>
2013-06-21 18:28:27 -0400

    Added beginnings of updatePlantMood() and generateResponse() with some blocked out pseudocode

--- server/server.js
+++ server/server.js
@@ -394,3 +394,89 @@ function Connection(_id, _device_id,_socket){
 	this.socket = _socket;	
 }
 
+/* ******************************************************************************************* */
+/* Update the plant's mood based on interaction & envrionmental data 						   */
+/* ******************************************************************************************* */
+function updatePlantMood(id) {
+
+	// fetch data from db for given device id
+	plantsDb.findOne({ '_id': new BSON.ObjectID(id) } ,function(err,result) {
+
+		// environmental data:
+		//
+		// moisture		dried out	|	good	|	wet
+		// humidity		dried out	|	good	|	wet
+		// temp 		cold		|	good	|	hot
+		// light 		too dark	|	good	|	too bright
+		//
+		//
+		// interaction data:
+		//
+		// touch		lonely		|	happy	|	worked up
+
+		// need to pull thresholds for each data type from somewhere
+
+		// set state variables to -1, 0, or 1 depending on where they fall wrt their thresholds
+		// magic
+		moistureState = moistureVal < moistureLowThreshold ? -1 : val < moistureHighThreshold ? 0 : 1;
+		humidityState = humidityVal < humidityLowThreshold ? -1 : val < humidityHighThreshold ? 0 : 1;
+		tempState = tempVal < tempLowThreshold ? -1 : val < tempHighThreshold ? 0 : 1;
+		lightState = lightVal < lightLowThreshold ? -1 : val < lightHighThreshold ? 0 : 1;
+
+		touchState = touchVal < touchLowThreshold ? -1 : val < touchHighThreshold ? 0 : 1;
+		// (nested ternaries ftw)
+
+		// we're prioritizing touch & attention over environmental data
+		if (touchState == -1) {
+			// here, not enough attention
+			mood = "lonely";
+		} else if (touchState == 1) {
+			// too much attention
+			mood = "workedup";
+		} else if (touchState == 0) {
+			
+			// good amount of attention, we're happy
+			// now look to environmental data to see what it needs
+			
+			if (moistureState == -1) {
+				mood = "dry";
+			} else if (moistureState == 1) {
+				mood = "wet";
+			} else if (moistureState == 0) {
+				mood = "happy";
+			}
+
+		}
+	});
+}
+
+/* ******************************************************************************************* */
+/* Generate a response based on the plant's mood 								 			   */
+/* ******************************************************************************************* */
+function generateResponse(id) {
+
+	// fetch data from db for given device id
+	plantsDb.findOne({ '_id': new BSON.ObjectID(id) } ,function(err,result) {
+
+		if (result.mood == "lonely") {
+			// lonely response
+		}
+		if (result.mood == "workedup") {
+			// worked up response
+		}
+		if (result.mood == "dry") {
+			// dry response
+		}
+		if (result.mood == "wet") {
+			// wet/soggy response
+		}
+		if (result.mood == "happy") {
+			// basic needs of attention & water met
+			// responses could...
+			// thank user for fulfilling a need (attention, water)
+			// comment on weather (hot, dry, humid)
+			// provide a factoid
+		}
+		
+	});
+}
