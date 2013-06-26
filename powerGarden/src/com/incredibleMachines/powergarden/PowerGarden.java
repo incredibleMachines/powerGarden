301eb40a74660a07541ca388f6f6de44bc6dc670
Leif Percifield <lpercifield@gmail.com>
2013-06-26 14:12:05 -0400

    Merged layout and powergarden.java from chris branch

--- powerGarden/src/com/incredibleMachines/powergarden/PowerGarden.java
+++ powerGarden/src/com/incredibleMachines/powergarden/PowerGarden.java
@@ -4,8 +4,21 @@ import android.net.Uri;
 import android.widget.TextView;
 
 public class PowerGarden {
-	
+
 	public static SocketManager SM;
+	public static int Connected = 1;
+	public static int Disconnected = 2;
+	public static int Planted = 3;
+	
+	
+	public static class Device{
+		
+		static String ID;
+		static String connectionID;
+		static String PlantNum;
+		
+	}
+	
 	public static int temp;
 	public static int hum;
 	public static int light;
