edb3acd5a2aa457701b68cc919d53d2f97a8b39d
chris.piuggi <chris@piuggi.com>
2013-06-26 11:06:54 -0400

    Updating Server to run as class

--- server/server.js
+++ server/server.js
@@ -10,10 +10,12 @@ var WebSocketServer = require('ws').Server
 
 var io = require('socket.io').listen(9001);
   
+/*
 var MongoClient = require('mongodb').MongoClient,
 	Server = require('mongodb').Server,
 	mongo = new MongoClient(new Server('localhost', 27017)),
 	BSON = require('mongodb').BSONPure;
+*/
 
 var ntwitter = require('ntwitter');
 
@@ -22,7 +24,7 @@ var ntwitter = require('ntwitter');
 /* Define globals								 								 			   */
 /* ******************************************************************************************* */
 
-var db, dataDb, personalitiesDb, devicesDb, plantsDb, touchesDb;
+//var db, dataDb, personalitiesDb, devicesDb, plantsDb, touchesDb;
 var clients = {};
 var twitter;
 
@@ -31,6 +33,7 @@ var twitter;
 /* Connect to mongo server, store collection references							 			   */
 /* ******************************************************************************************* */
 
+/*
 mongo.open(function(err,mongo){
 	db = mongo.db('powergarden');
 	devicesDb = db.collection('devices');
@@ -39,7 +42,10 @@ mongo.open(function(err,mongo){
 	dataDb = db.collection('data');
 	touchesDb = db.collection('touches');
 });
+*/
 
+var DB = require('./db/dbInterface');
+var database = new DB();
 
 /* ******************************************************************************************* */
 /* Configure twitter stream & connect											 			   */
@@ -98,9 +104,9 @@ twitter.stream('statuses/filter', { track: twitterTrackString, stall_warnings: t
 
 	// Deal with errors
 	stream.on('error', function(error, code){
-		console.log('[Twitter Stream] Error: ' + code);
-		console.log(error);
-		throw error;
+		console.error('[Twitter Stream] Error: ' + code);
+		console.error(error);
+		//throw error;
 	});
 	stream.on('end', function (response) {
 		// Handle a disconnection
@@ -129,211 +135,36 @@ io.sockets.on('connection', function (socket) {
 	console.log("[NEW CONN] connection.id %s",connection.id);
 	console.log("[NEW CONN] connection.device_id %s",connection.device_id);
 
-
 	socket.on('register', function (msg) {
 	    console.log('[Device Register Request]: ', msg);
-	    routeRegister(msg,connection);
+	    database.routeRegister(msg,connection);
 	  
 	});
+	
 	socket.on('update', function (msg) {
 	    console.log('[Device Update Request]: ', msg);
-	    routeUpdate(msg,connection);
+	    database.routeUpdate(msg,connection);
 	  
 	});
-	  
+	
 	socket.on('touch',function(msg){
-		  console.log('[Plant Touch Signaled]', msg);
-		  plantTouch(msg,connection);
+		console.log('[Plant Touch Signaled]', msg);
+		database.plantTouch(msg,connection);
 		  
 	});
 	
 	socket.on('disconnect', function () {
-	    /* io.sockets.emit('user disconnected'); */
+	  /* io.sockets.emit('user disconnected'); */
 	  delete clients["client-"+connection.id];
 	  console.log("[DISCONN] connection.id %s",connection.id);
 	  console.log("[DISCONN] connection.device_id %s",connection.device_id);
+	  //set device to inactive
 
-	  delete clients["client-"+connection.id];
 	});
 });
 
 /* ******************************************************************************************* */
-/* ******************************************************************************************* */
-
-function routeUpdate(message,connection){
-	
-	if(message.device_id != connection.device_id) { 
-		console.log('[Update Error] message.device_id=%s connection.device_id=%s', message.device_id, connection.device_id );/*error error;*/ 
-	}else{
-		 
-		console.log("GOT MESSAGE.DATA :: "+JSON.stringify(message.data));
-			
-		var obj = {	'device_id': new BSON.ObjectID( String(message.device_id)), 
-					'moisture':message.data.moisture, 
-					'temp': message.data.temp, 
-					'humidity':message.data.humidity, 
-					'light': message.data.light,
-					'timestamp': new Date()
-				};
-				
-		dataDb.insert(obj, {safe:true}, function(err,doc){
-			if(err) throw err;
-			
-			//check the last values and determine mood of plant 
-			//update plants/device if necessary
-		
-		});	
-	}
-}
-
-/* ******************************************************************************************* */
-/* ******************************************************************************************* */
-
-function routeRegister(message,connection){
-	//check for ID in DB
-	//if ID exists
-	//return ID, status - connected, rejoined
-	if(message.device_id == "set_id"){
-	
-		console.log("New Device - Logging Now");		
-		logDevice(message,connection);
-	
-	}else{
-		console.log("Size of device_id: "+message.device_id.length);
-		
-		if(message.device_id.length == 24){
-			var oID = new BSON.ObjectID(message.device_id);
-			var obj = {'_id': oID};
-			//find device by id
-			devicesDb.findOne(obj,function(err,result){
-				
-				if(!result){
-					console.log("[Device Registration Failed]");
-					logDevice(message,connection);
-					
-				}else{
-					
-					var res = { "device_id": message.device_id, "connection_id": connection.id };
-					console.log("[Device Already Registered]");
-					connection.device_id = message.device_id;
-					//assignPlantData(result,connection);
-					
-					connection.socket.emit('connected', res);
-					//connection.socket.send(JSON.stringify(res));	
-				}
-				
-			});
-			
-		}else{
-			
-			logDevice(message,connection);
-
-		}
-		//check plants and register them
-		
-	}
-}
-
-/* ******************************************************************************************* */
-/* ******************************************************************************************* */
-
-function assignPlantData(result,connection){
-	//result is a db document of device
-	//console.log(result.plants);
-	for(var i = 0; i< result.plants.length; i++){
-		console.log(result.plants[i]._id);		
-/*
-		var oID = new BSON.ObjectID(result.plants[i]._id);*/
-		var obj = {'_id': result.plants[i]._id };
-		plantsDb.findOne(obj,function(err,result){
-		
-					var res = {	//"status": "planted", 
-								"device_id": connection.device_id, 
-								"connection_id": connection.id, 
-								"plant":{"id": result._id, "type":result.type, "index":result.index , "mood":result.mood } 
-					};
-					//connection.socket.send(JSON.stringify(res));
-					connection.socket.emit('planted',res);
-		});	
-	}	
-}
-
-/* ******************************************************************************************* */
-/* ******************************************************************************************* */
-
-function logDevice(message,connection){
-	var obj = {date: new Date(), plants: []};
-	devicesDb.insert(obj, {safe:true}, function(err,doc){
-		if(err)throw err;
-		
-		connection.device_id = doc[0]._id;
-		console.log('Created Record: '+connection.device_id);
-		console.log("plants.length: "+message.num_plants);
-		for(var i = 0; i<message.num_plants; i++) createPlant(message,connection,i);
-		
-		
-		var res = { "device_id": connection.device_id, "connection_id": connection.id };
-		//connection.socket.send(JSON.stringify(res));
-		connection.socket.emit('connected',res);
-	});	
-}
-
-/* ******************************************************************************************* */
-/* ******************************************************************************************* */
-//update mongo db document
-function updateDocument(collection,id,json){
-	console.log("[Updating Document] Collection: "+collection+" id: "+id);
-	var obj = { _id : new BSON.ObjectID( String(id) ) };
-	collection.update(obj,json,function(err){
-		if(err)throw err;
-	});
-
-//no upsert 
-}
-
-/* ******************************************************************************************* */
-/* ******************************************************************************************* */
-
-function createPlant(message,connection,plant_index){
-	//blocking or non-blocking function?
-	
-	//console.log("plant: "+ JSON.stringify(plant));
-	console.log('[CREATING PLANT]');
-	var obj = {created: new Date(), device_id:connection.device_id, index: plant_index, type: message.plant_type, mood: "born", touch:0 };
-	plantsDb.insert(obj,{safe:true},function(err,doc){
-			if(err) throw err;
-
-		   var json={ $push: { plants: { index:doc[0].index, _id:doc[0]._id } } };
-		   updateDocument(devicesDb,connection.device_id, json);
-		   
-	});
-}
-
-/* ******************************************************************************************* */
-/* ******************************************************************************************* */
-
-function plantTouch(message,connection){
-	
-	//May need to create dual index of device_id & plant index
-
-	var obj = {'device_id': new BSON.ObjectID(String(message.device_id)), 'index': message.plant_index };
-	var json = {$inc : {touch: 1}};
-	plantsDb.update(obj, json,function(err){
-		if(err) throw err;
-		//check & respond w mood update.
-
-	});	
-	
-	obj.timestamp = new Date();
-		
-	touchesDb.insert(obj, function(err){
-		if(err) throw err;
-	});
-
-	twitterRef.gotTouched();
-}
-
-/* ******************************************************************************************* */
+/*																							   */
 /* ******************************************************************************************* */
 function Connection(_id, _device_id,_socket){
 	//variables can be altered when  
@@ -350,8 +181,8 @@ function Connection(_id, _device_id,_socket){
 function checkPlantMood(message,connection) {
 
 	// fetch data from db for given device id
-	plantsDb.findOne({ '_id': new BSON.ObjectID(message.device_id) } ,function(err,result) {
-		if (err) throw err;
+	plantsDb.findOne({ 'device_id': new BSON.ObjectID(message.device_id), 'index': message.plant_index } ,function(err,result) {
+		if (err)  console.error(err); //throw err;
 		if (!result) { console.log('[Check Plant Mood] No plant found for given id: ' + message.device_id); return; }
 		
 		// environmental data:
@@ -408,8 +239,8 @@ function checkPlantMood(message,connection) {
 function generateResponse(message,connection) {
 
 	// fetch data from db for given device id
-	plantsDb.findOne({ '_id': new BSON.ObjectID(message.device_id) } ,function(err,result) {
-		if (err) throw err;
+	plantsDb.findOne({ '_id': new BSON.ObjectID(message.device_id), 'index': message.plant_index  } ,function(err,result) {
+		if (err) console.error(err); //throw err;
 		if (!result) { console.log('[Generate Response] No plant found for given id: ' + message.device_id); return; }
 
 		if (result.mood == "lonely") {
---
+++ server/touchSimulator.js
@@ -0,0 +1,42 @@
+var MongoClient = require('mongodb').MongoClient,
+	Server = require('mongodb').Server,
+	mongo = new MongoClient(new Server('localhost', 27017)),
+	BSON = require('mongodb').BSONPure;
+	
+	
+mongo.open(function(err,mongo){
+	
+	if(err) console.error(err);
+	
+	db = mongo.db('powergarden');
+	devicesDb = db.collection('devices');
+	plantsDb = db.collection('plants');
+	personalitiesDb = db.collection('personalities');
+	dataDb = db.collection('data');
+	touchesDb = db.collection('touches');
+});
+
+
+setInterval(simulateTouch, 1000 );
+
+var devices = [	"51ca511efaea9671b1000037", 				
+				"51ca5129faea9671b1000040",
+				"51ca513dfaea9671b1000049",
+				"51ca514afaea9671b1000052",
+				"51ca5153faea9671b100005b",
+				"51ca515bfaea9671b1000064",
+				"51ca5166faea9671b100006d",
+				"51ca517afaea9671b1000076"];
+				
+function simulateTouch(){
+	
+	
+	var obj ={};
+	var randDevice = devices[Math.round(Math.random()*devices.length)];
+	obj.device_id = new BSON.ObjectID(randDevice);
+	obj.index = Math.round(Math.random()*8);
+	obj.timestamp = new Date();
+	touchesDb.insert(obj, {safe:true}, function(err,res){ if(err)console.error(err); });
+	console.log(obj);
+	
+};
