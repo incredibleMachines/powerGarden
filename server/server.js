0c0754c4e7aadafac525d6c0ae4a5c49438c2b50
chris.piuggi <chris@piuggi.com>
2013-07-02 16:41:34 -0400

    adding plant_slug to Connection and mood calculations

--- server/server.js
+++ server/server.js
@@ -66,6 +66,7 @@ browserio.sockets.on('connection',function(browserSocket){
 	
 		browserSocket.emit('init', {connection_id: clients[key]['id'], device_id: clients[key]['device_id']});	
 	}
+	
 	//browserSocket.emit('init', clients);
 	browserSocket.on('threshold',function(msg){
 		console.log(msg);
@@ -172,107 +173,9 @@ function Connection(_id, _device_id,_socket){
 	this.id = _id;
 	this.device_id = _device_id;
 	this.socket = _socket;	
+	this.plant_type;
+	this.plant_slug;
 }
-
-/* ******************************************************************************************* */
-/* Update the plant's mood based on interaction & envrionmental data 						   */
-/* ******************************************************************************************* */
-
-/*
-function checkPlantMood(message,connection) {
-
-	// fetch data from db for given device id
-	plantsDb.findOne({ 'device_id': new BSON.ObjectID(message.device_id), 'index': message.plant_index } ,function(err,result) {
-		if (err)  console.error(err); //throw err;
-		if (!result) { console.log('[Check Plant Mood] No plant found for given id: ' + message.device_id); return; }
-		
-		// environmental data:
-		//
-		// moisture		dried out	|	good	|	wet
-		// humidity		dried out	|	good	|	wet
-		// temp 		cold		|	good	|	hot
-		// light 		too dark	|	good	|	too bright
-		//
-		//
-		// interaction data:
-		//
-		// touch		lonely		|	happy	|	worked up
-
-		// need to pull thresholds for each data type from somewhere
-
-		// set state variables to -1, 0, or 1 depending on where they fall wrt their thresholds
-		// magic
-		moistureState = moistureVal < moistureLowThreshold ? -1 : val < moistureHighThreshold ? 0 : 1;
-		humidityState = humidityVal < humidityLowThreshold ? -1 : val < humidityHighThreshold ? 0 : 1;
-		tempState = tempVal < tempLowThreshold ? -1 : val < tempHighThreshold ? 0 : 1;
-		lightState = lightVal < lightLowThreshold ? -1 : val < lightHighThreshold ? 0 : 1;
-
-		touchState = touchVal < touchLowThreshold ? -1 : val < touchHighThreshold ? 0 : 1;
-		// (nested ternaries ftw)
-
-		// we're prioritizing touch & attention over environmental data
-		if (touchState == -1) {
-			// here, not enough attention
-			mood = "lonely";
-		} else if (touchState == 1) {
-			// too much attention
-			mood = "workedup";
-		} else if (touchState == 0) {
-
-			// good amount of attention, we're happy
-			// now look to environmental data to see what it needs
-
-			if (moistureState == -1) {
-				mood = "dry";
-			} else if (moistureState == 1) {
-				mood = "wet";
-			} else if (moistureState == 0) {
-				mood = "happy";
-			}
-
-		}
-	});
-}
-*/
-
-
-/* ******************************************************************************************* */
-/* Generate a response based on the plant's mood 								 			   */
-/* ******************************************************************************************* */
-
-function generateResponse(message,connection) {
-
-	// fetch data from db for given device id
-	plantsDb.findOne({ '_id': new BSON.ObjectID(message.device_id), 'index': message.plant_index  } ,function(err,result) {
-		if (err) console.error(err); //throw err;
-		if (!result) { console.log('[Generate Response] No plant found for given id: ' + message.device_id); return; }
-
-		if (result.mood == "lonely") {
-			// lonely response
-		}
-		if (result.mood == "workedup") {
-			// worked up response
-		}
-		if (result.mood == "dry") {
-			// dry response
-		}
-		if (result.mood == "wet") {
-			// wet/soggy response
-		}
-		if (result.mood == "happy") {
-			// basic needs of attention & water met
-			// responses could...
-			// thank user for fulfilling a need (attention, water)
-			// comment on weather (hot, dry, humid)
-			// provide a factoid
-		}
-	});
-}
-
-
-/* ******************************************************************************************* */
-/* Callback for dealing with processed stream data 								 			   */
-/* ******************************************************************************************* */
 
 function twitterCallback(data) {
 
