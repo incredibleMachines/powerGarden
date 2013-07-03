3663d96f61ab1b02c5c4ab59deb0188495dcdd92
Matt Felsen <mattfelsen@gmail.com>
2013-07-03 01:54:43 -0400

    Prefix all pump log messages with [PUMP]

--- server/pump/index.js
+++ server/pump/index.js
@@ -13,39 +13,39 @@ net.createServer(function (socket) {
  
   // Identify this client
   socket.name = socket.remoteAddress + ":" + socket.remotePort 
-  console.log('Connection: ' + socket.name);
+  console.log('[PUMP] Connection: ' + socket.name);
  
   // Put this new client in the list
   pumpClients.push(socket);
-  console.log('Clients: ' + pumpClients.length)
+  console.log('[PUMP] Number of clients: ' + pumpClients.length)
  
   // Send a nice welcome message and announce
   // socket.write("Welcome " + socket.name + "\n");
  
   // Handle incoming messages from pumpClients.
   socket.on('data', function (data) {
-    console.log(socket.name + ': ' + data);
+    console.log('[PUMP] data: ' + socket.name + ': ' + data);
   });
 
   socket.on('error', function(error) {
   	if (error.code == "ECONNRESET") {
-	    console.log('Disconnection: ' + socket.name);
+	    console.log('[PUMP] Error: Disconnection: ' + socket.name);
 	    pumpClients.splice(pumpClients.indexOf(socket), 1);
-	    console.log('Clients: ' + pumpClients.length)
+	    console.log('[PUMP] Number of clients: ' + pumpClients.length)
   	}
   });
  
   // Remove the client from the list when it leaves
   socket.on('end', function () {
-    console.log('Disconnection: ' + socket.name);
+    console.log('[PUMP] End: Disconnection: ' + socket.name);
     pumpClients.splice(pumpClients.indexOf(socket), 1);
-    console.log('Clients: ' + pumpClients.length)
+    console.log('[PUMP] Number of clients: ' + pumpClients.length)
   });
 
  
 }).listen(9001);
  
-console.log("Server running");
+console.log("[PUMP] Server running");
 
 // setInterval(turnOnSprinklers, 10*1000);
 
@@ -53,7 +53,7 @@ function writeToClents(message) {
 	if (!pumpClients.length) return;
 
 	for (var i = 0; i < pumpClients.length; i++) {
-		console.log("Checking "+ i +" of "+ pumpClients.length);
+		console.log("[PUMP] Writing, checking index "+ i +" of length "+ pumpClients.length);
 		var socket = pumpClients[i];
 
 		socket.write(message);
@@ -64,7 +64,7 @@ exports.turnOnSprinklers = function(_duration) {
 	var duration = _duration || 15;
 	var message = '1,' + duration * 1000;
 	writeToClents(message);
-	console.log('['+pumpClients.length+'] Turning on sprinklers...');
+	console.log('[PUMP] ['+pumpClients.length+'] Turning on sprinklers...');
 
 	clearTimeout(turnOffTimer);
 	turnOffTimer = setTimeout(this.turnOffSprinklers, duration * 1000);
@@ -73,5 +73,5 @@ exports.turnOnSprinklers = function(_duration) {
 exports.turnOffSprinklers = function() {
 	var message = '0,0';
 	writeToClents(message);
-	console.log('['+pumpClients.length+'] Turning off sprinklers!');
+	console.log('[PUMP] ['+pumpClients.length+'] Turning off sprinklers!');
 }
